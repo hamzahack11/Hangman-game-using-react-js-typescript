f6010a247f8ffdfaccb0db061791d6e7f5672531
hamzahack11 <hamzahack11@gmail.com>
2023-03-09 13:14:10 -0500

    add hangman

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Hangman-Game-React-JS-TypeScript/",
+  base: "/Hangman-game-using-react-js-typescript/",
   plugins: [react()],
 })
